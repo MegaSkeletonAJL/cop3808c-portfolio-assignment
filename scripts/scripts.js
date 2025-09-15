a1db3aa76be1fdf620f0b3631fee308fdf3d1095
MegaSkeletonAJL <retoman7@protonmail.com>
2025-09-14 22:38:11 -0400

    add a javascript for outputting messages

---
+++ scripts/scripts.js
@@ -0,0 +1,22 @@
+// call function inside const variable for message output
+const toast = {
+	initialize() {
+		this.hideTimeout = null;
+		// Create toast div and append it to body
+		this.element = document.createElement('div');
+		this.element.className = 'toast';
+		document.body.appendChild(this.element);
+	},
+
+	showToast(message) {
+		clearTimeout(this.hideTimeout);
+
+		this.element.textContent = message;
+		this.element.className = 'toast toast-show';
+		// remove toast-show class after specified milliseconds
+		this.hideTimeout = setTimeout(() => {
+			this.element.classList.remove('toast-show');
+		}, 4000);
+	},
+};
+
